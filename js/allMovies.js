78b3842fcb135b9fcd81f654eb733b8354456b2f
Chrilleweb <christian.munknissen@gmail.com>
2023-10-02 22:54:48 +0200

    front page with image looks better

--- js/allMovies.js
+++ js/allMovies.js
@@ -25,21 +25,48 @@ function displayMovies(movies) {
     // Loop through each movie and create list items
     movies.forEach((movie) => {
         const listItem = document.createElement("li");
+        listItem.style.display = "inline-block"; // Set display to inline-block for side-by-side layout
+        listItem.style.marginRight = "10px"; // Add some margin between each movie
 
         // Create an image element and set its source to the movie's imageUrl
         const imageElement = document.createElement("img");
-        imageElement.src = movie.imageUrl;
-        imageElement.alt = movie.title;  // Set alt text for accessibility
-        imageElement.style.width = "100px";  // Set a fixed width for the image
+        imageElement.src = movie.imageUrl || "https://media.comicbook.com/files/img/default-movie.png"; // Use default image if imageUrl is not available
+        imageElement.alt = movie.title; // Set alt text for accessibility
+        imageElement.style.width = "100px"; // Set a fixed width for the image
 
-        // Append the image and other details to the list item
+        // Append the image to the list item
         listItem.appendChild(imageElement);
-        listItem.innerHTML += `Title: ${movie.title} - Category: ${movie.category} - Age Limit: ${movie.ageLimit} - Duration: ${movie.duration} minutes`;
+
+        // Create a div for movie details
+        const detailsDiv = document.createElement("div");
+
+        // Append title to detailsDiv
+        const titleElement = document.createElement("p");
+        titleElement.textContent = movie.title;
+        detailsDiv.appendChild(titleElement);
+
+        // Append category, age limit, and duration under the title
+        const categoryElement = document.createElement("p");
+        categoryElement.textContent = `Category: ${movie.category}`;
+        detailsDiv.appendChild(categoryElement);
+
+        const ageLimitElement = document.createElement("p");
+        ageLimitElement.textContent = `Age Limit: ${movie.ageLimit}`;
+        detailsDiv.appendChild(ageLimitElement);
+
+        const durationElement = document.createElement("p");
+        durationElement.textContent = `Duration: ${movie.duration} minutes`;
+        detailsDiv.appendChild(durationElement);
+
+        // Append detailsDiv to the list item
+        listItem.appendChild(detailsDiv);
 
         // Append the list item to the movie list
         movieList.appendChild(listItem);
     });
 }
+
+
 
 // Fetch and display all movies when the page loads
 document.addEventListener("DOMContentLoaded", getAllMovies);
