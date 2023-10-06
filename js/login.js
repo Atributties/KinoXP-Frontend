d187dbd639c2e0f5b9fb3fc32ad2bcea67753558
Chrilleweb <christian.munknissen@gmail.com>
2023-10-06 10:39:18 +0200

    can see role and go to costumerpage and employeepage

--- js/login.js
+++ js/login.js
@@ -7,35 +7,62 @@ const loginForm = document.getElementById("login-form")
 async function login(event) {
     event.preventDefault();
     try {
-
         const user = {
             email: document.getElementById("email").value,
             password: document.getElementById("password").value,
-
         };
-        const resp = await postObjectAsJson(url, user, "POST")
+        const resp = await postObjectAsJson(url, user, "POST");
+
+        // ... (previous code)
 
         if (resp.ok) {
-            // Successful login, handle accordingly
-            alert('Login successful!');
+            const responseText = await resp.text();
+
+            if (responseText.includes("Login successful")) {
+                // Fetch the user's role using the /user/role/{email} endpoint
+                const roleResp = await fetch(`http://localhost:8080/user/role/${user.email}`);
+                if (roleResp.ok) {
+                    const roleData = await roleResp.json();
+                    console.log('Role data:', roleData);
+                    const userRole = roleData || "CUSTOMER";
+
+                    // Redirect based on the user role
+                    if (userRole === "CUSTOMER") {
+                        window.location.href = "costumerPage.html";
+                    } else if (userRole === "ADMIN" || userRole === "Employee") {
+                        window.location.href = "index.html";
+                    } else {
+                        alert('Unknown role. Unable to redirect.');
+                    }
+                } else {
+                    // Handle error when fetching user role
+                    console.error('Error fetching user role:', roleResp.statusText);
+                    alert('Error fetching user role');
+                }
+            } else {
+                alert('An error occurred');
+            }
+
         } else if (resp.status === 400) {
-            // Invalid credentials
             alert('Invalid username or password');
         } else if (resp.status === 404) {
-            // User not found
             alert('User not found');
         } else {
-            // Handle other error cases
             alert('An error occurred');
         }
+
         loginForm.reset(); // Reset the form
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
+loginForm.addEventListener('submit', login);
 
-loginForm.addEventListener('submit', login)
+
+
+
+
 
 
 
